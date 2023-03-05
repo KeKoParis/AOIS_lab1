4aceb14470f6036a99381b4825dd290ff05e2113
Stanislav Voitkus <stanislav.voitkus@outlook.com>
2023-03-05 14:07:19 +0300

    console input

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"lab1/bno"
 	"lab1/sm"
@@ -8,25 +9,29 @@ import (
 
 func main() {
 
-	c := 5.155
-	b := 3.25
+	firstNumber := flag.Float64("firstNumber", 5.155, "user's first number")
+	secondNumber := flag.Float64("secondNumber", 3.25, "user's second number")
+
+	firstValue := *firstNumber
+	secondValue := *secondNumber
+
 	var binFloat [][]float64
-	binFloat = bno.Summation(bno.ConvertToBin(c), bno.ConvertToBin(b))
+	binFloat = bno.Summation(bno.ConvertToBin(firstValue), bno.ConvertToBin(secondValue))
 	fmt.Println(binFloat, "  len mantissa ", len(binFloat[2]))
 
-	fmt.Println(bno.ConvertToDec(bno.Summation(bno.ConvertToBin(c), bno.ConvertToBin(b))))
+	fmt.Println(bno.ConvertToDec(bno.Summation(bno.ConvertToBin(firstValue), bno.ConvertToBin(secondValue))))
 
-	c = 27
-	b = 2
+	firstValue = 27
+	secondValue = 2
 	var firstNum [16]float64
 	var secondNum [16]float64
 
-	arr := sm.Convert(c)
+	arr := sm.Convert(firstValue)
 	for i := 0; i < 16; i++ {
 		firstNum[i] = arr[i]
 	}
 
-	arr = sm.Convert(b)
+	arr = sm.Convert(secondValue)
 	for i := 0; i < 16; i++ {
 		secondNum[i] = arr[i]
 	}
